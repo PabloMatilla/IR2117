497f2bec5e99befa58709138017cfa2e698dc44d
PabloMatilla <al415550@uji.es>
2023-01-26 12:56:41 +0100

    Programa sum v 1.2

--- Suma/sum.cpp
+++ Suma/sum.cpp
@@ -4,6 +4,9 @@ int main() {
     int n, suma = 0;
     std::cout << "Enter a number: ";
     std::cin >> n;
+    for (int i = 0; i <= n; i++) {
+        suma += i;
+    }
     std::cout << suma << std::endl;
     return 0;
 }
