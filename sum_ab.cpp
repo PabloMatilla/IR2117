7c7d6d0ef46edb671ebc9200c82eb9a7d6ed5dcc
Pablo Expósito <al415550@uji.es>
2023-01-31 12:36:16 +0100

    Cuarta versión sum_ab.cpp

---
+++ sum_ab.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+int main() {
+    int a = 0, b = 0, suma = 0;
+    while (a < 1) {
+        std::cout << "Introduzca el primer valor (a): ";
+        std::cin >> a;
+    }
+    while (b < a) {
+        std::cout << "Introduzca el segundo valor (b): ";
+        std::cin >> b;
+    }
+
+    for (int i = a; i <= b; i++) {
+        suma += i;
+    }
+    std::cout << "La suma es " << suma << std::endl;
+    return 0;
+}
