5cb994ed39b636552e3a50b47cffc7d1e96e5a99
Pablo Expósito <al415550@uji.es>
2023-01-31 21:20:59 +0100

    Quinta versión estadistico (calculo de recoorido, varianza y desv. tipica

--- estadisticos.cpp
+++ estadisticos.cpp
@@ -6,7 +6,7 @@ int main() {
     std::cout << "El programa realiza los principales estadísticos de una muestra" << std::endl;
     int n_datos;
     std::vector<double> vector;
-    double media, mediana, recorrido, recorrido_intercuartilico, varianza;
+    double media, mediana, recorrido, varianza;
     double desviacion_tipica, cuasivarianza, cuasidesviacion_tipica, cv;
     std::cout << "Introduzca la cantidad de datos de la muestra: ";
     std::cin >> n_datos;
@@ -30,10 +30,13 @@ int main() {
         mediana = vector[(n_datos)/2];
     }
 
+    // Calculo de varianza
+    for (int i = 0; i < n_datos; i++) {
+        varianza = vector[i]^2;
+    }
     std::cout << "Media: " << media << std::endl;
     std::cout << "Mediana: " << mediana << std::endl;
     std::cout << "Recorrido: " << recorrido << std::endl;
-    std::cout << "Recorrido intercuartilico: " << recorrido_intercuartilico << std::endl;
     std::cout << "Varianza: " << varianza << std::endl;
     std::cout << "Desviacion tipica: " << desviacion_tipica << std::endl;
     std::cout << "Cuasivarianza: " << cuasivarianza << std::endl;
