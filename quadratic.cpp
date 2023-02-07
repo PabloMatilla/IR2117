526fdd266d03d35ac13c24818a83b22b806bf1b6
Pablo Expósito <al415550@uji.es>
2023-02-07 12:51:27 +0100

    Primera versión quadratic

---
+++ quadratic.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Programa para buscar la solucion de una ecuacion cuadrada ax^2 + bx + c = 0" << std::endl;
+    double a, b, c, sol;
+    std::cout << "Introduce el valor de a: ";
+    std::cin >> a;
+    std::cout << "Introduce el valor de b: ";
+    std::cin >> b;
+    std::cout << "Introduce el valor de c: ";
+    std::cin >> c;
+
+    sol = 0;
+
+    std::cout << "La solucion es x = " << sol << std::endl;
+
+
+}
