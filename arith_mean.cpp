02e37ae45377107e0f5df75b2f89e331e6dc7f9c
PabloMatilla <al415550@uji.es>
2023-02-14 12:24:03 +0100

    Séptima versión arith_mean

--- arith_mean.cpp
+++ arith_mean.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
+#include <vector>
 
 int main() {
    double m = 0, s = 0;
-   int num, n = 0;
-   std::cin >> num;
+   int element, n = 0;
+   std::vector<int> elements;
+   std::cin >> element;
    while(!std::cin.eof()) {
-       s += num;
+       elements.push_back(element);
        n++;
-       std::cin >> num;
+       std::cin >> element;
    }
+   for (int i = 0; i < n; i++)
+       s += elements[i];
    m = s/n;
    std::cout << "Aritmetric mean: " << m << std::endl;
    return 0;
