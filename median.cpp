44020a35bdadddf12d8caffeb9fb233501657e1e
PabloMatilla <al415550@uji.es>
2023-02-14 12:47:42 +0100

    Version 1 median

--- median.cpp
+++ median.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 int main() {
-   double m = 0, s = 0;
+   double m = 0;
    int element, n = 0;
    std::vector<int> elements;
    std::cin >> element;
@@ -11,6 +12,17 @@ int main() {
        n++;
        std::cin >> element;
    }
+   std::sort(elements.begin(), elements.end());
+   if (n % 2 == 0) { // Impar
+        m = double(elements[n/2 -1] + elements[n/2])/2;
+       std::cout << "Even" << std::endl;
+   } else { // Par
+        m = elements[n/2];
+       std::cout << "Odd" << std::endl;
+
+   }
+
+
    std::cout << "Median: " << m << std::endl;
    return 0;
 }
