6c913f714fd319a08e553ecdee8e1f56b089dee5
PabloMatilla <al415550@uji.es>
2023-02-14 11:35:31 +0100

    Tercera versión arith_mean

--- arith_mean.cpp
+++ arith_mean.cpp
@@ -4,9 +4,9 @@ int main(int argc, char** argv){
     double m = 0, s = 0;
     int n = argc -1;
     for (int i = 1; i <= n; i++)
-        s += atgv[i];
+        s += atoi(argv[i]);
     m = s/n;
 
-    std::cout << "Arithmetic mean: " << m << std::cout;
+    std::cout << "Arithmetic mean: " << m << std::endl;
     return 0;
 }
