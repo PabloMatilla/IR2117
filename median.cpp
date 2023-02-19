747ac01719c39433332de4f70004c7ad630f7c71
Pablo Expósito <al415550@uji.es>
2023-02-20 00:02:46 +0100

    Test for even/odd

--- median.cpp
+++ median.cpp
@@ -14,9 +14,9 @@ int main(int argc, char** argv) {
     }
     std::sort(elements.begin(), elements.end());
 
-    if (n % 2 == 0) { // Impar
-        std::cout << "Even" << std::endl;
-    } else { // Par
+    if (n % 2 == 0) {
+        m = double(elements[n/2 -1] + elements[n/2])/2;
+    } else {
         std::cout << "Odd" << std::endl;
     }
 
