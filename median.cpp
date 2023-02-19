8c11b26a43658299d1ff52e7ddb7f25cdbb7c0a8
Pablo Expósito <al415550@uji.es>
2023-02-19 23:53:26 +0100

    Version 0 median

---
+++ median.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+int main(int argc, char** argv) {
+    double m = 0;
+    int element, n = 0;
+    std::vector<int> elements;
+    std::cin >> element;
+    while(!std::cin.eof()) {
+        elements.push_back(element);
+        n++;
+        std::cin >> element;
+    }
+    std::cout << "Median: " << m << std::endl;
+    return 0;
+}
