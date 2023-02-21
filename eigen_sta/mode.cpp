9b4eda5843929b3e1db9e8c1b96339721ca9eea2
PabloMatilla <al415550@uji.es>
2023-02-21 22:32:46 +0100

    Update mode.cpp

--- eigen_sta/mode.cpp
+++ eigen_sta/mode.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
 #include <Eigen/Dense>
+#include <map>
 #include "utils.hpp"
 
 using Eigen::VectorXd;
 int main() {
     double m;
-    VectorXd elements = LeerElementosV();
+    VectorXd& elements = LeerElementosV();
+    std::map<int, int> dic;
+
+    for(auto e : elements) {
+        dic[e]++;
+    }
 
     std::cout << "Mode: " << m << std::endl;
     return 0;
