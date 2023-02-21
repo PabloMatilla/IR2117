a3af156b3ffc5fca4d497b07a669051c640bee73
PabloMatilla <al415550@uji.es>
2023-02-21 22:27:12 +0100

    Borrar odenar vector

--- eigen_sta/mode.cpp
+++ eigen_sta/mode.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <algorithm>
 #include <Eigen/Dense>
 #include "utils.hpp"
 
@@ -7,13 +6,6 @@ using Eigen::VectorXd;
 int main() {
     double m;
     VectorXd elements = LeerElementosV();
-    std::sort(elements.begin(), elements.end());
-
-    map m
-    for (double e : elements) {
-
-    }
-
 
     std::cout << "Mode: " << m << std::endl;
     return 0;
