bfa80a1b96fe407d9b220a9041f3dd590b708769
PabloMatilla <al415550@uji.es>
2023-02-28 11:46:33 +0100

    Pedir valores

--- suma_max_matrix.cpp
+++ suma_max_matrix.cpp
@@ -7,17 +7,18 @@ using Eigen::VectorXd;
 int main () {
     int n;
     double sumamax = 0;
-    std::cout << "Introduce el tamano de la matrix" << std::endl;
+    std::cout << "Introduce el tamano de la matrix: ";
     std::cin >> n;
 
     MatrixXd m(n, n);
-    for (int i = 0, i < n; i++) {
-        for (int j = 0, j < n; j++) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
             double element;
             std::cout << "Introduce el elemento ("<< i << "," << j << ")" << std::endl;
             std::cin >> element;
             m(i, j) = element;
         }
     }
-    std::cout << "La suma mayor es: " << sumamax << std::endlsu;
+    std::cout << "La matriz es: " << std::endl << m << std::endl;
+    std::cout << "La suma mayor es: " << sumamax << std::endl;
 }
