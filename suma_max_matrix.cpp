f32197f5a42805a556a16f7df15b80c7bcf86c58
PabloMatilla <al415550@uji.es>
2023-03-14 14:11:30 +0100

    input data

--- suma_max_matrix.cpp
+++ suma_max_matrix.cpp
@@ -5,22 +5,16 @@ using Eigen::MatrixXd;
 using Eigen::VectorXd;
 
 int main () {
-    int n;
-    double sumamax = 0;
-    std::cout << "Introduce el tamano de la matrix: ";
-    std::cin >> n;
+    int rows, cols;
+    std::cout << "Enter matrix size: ";
+    std::cin >> rows;
+    cols = rows;
+    cols = rows;
 
-    MatrixXd m(n, n);
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            double element;
-            std::cout << "Introduce el elemento ("<< i << "," << j << ")" << std::endl;
-            std::cin >> element;
-            m(i, j) = element;
-        }
-    }
-    std::cout << "La matriz es: " << std::endl << m << std::endl;
-    std::cout << "La suma mayor es: " << sumamax << std::endl;
-    double s = m.row(0).sum();
-    std::cout << "The sum of row 0 is " << s << std::endl;
+    MatrixXd m(rows, cols);
+    std::cout << "Enter elements (row-wise): " << std::endl;
+    for (int i = 0; i < rows; i++)
+        for (int j= 0; j < cols; j++)
+            std::cin >> m(i,j);
+    std::cout << "The matrix is: " << std::endl << m << std::endl;
 }
