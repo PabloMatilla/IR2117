3ef2bc5daeb17403718328d1870088da1a212d5e
PabloMatilla <al415550@uji.es>
2023-03-14 14:27:11 +0100

    get maximum of rows and columns

--- suma_max_matrix.cpp
+++ suma_max_matrix.cpp
@@ -22,4 +22,15 @@ int main () {
     for (int i = 0; i < rows; i++)
         s(i) = m.row(i).sum();
     std::cout << "The sum of rows is " << s << std::endl;
+
+    double maxRowSum = s.maxCoeff();
+    std::cout << "The maxium sum of rows is " << maxRowSum << std::endl;
+
+    for (int i = 0; i < cols; i++)
+        s(i) = m.col(i).sum();
+    std::cout << "The sum of columns is " << std::endl << s << std::endl;
+    double maxColSum = s.maxCoeff();
+    std::cout << "The maximun sum of columns is " << maxColSum << std::endl;
+    double maxRowColSum = maxRowSum > maxColSum ? maxRowSum : maxColSum;
+    std::cout << "The maximum sum of rows or colums is " << maxRowColSum << std::endl;
 }
