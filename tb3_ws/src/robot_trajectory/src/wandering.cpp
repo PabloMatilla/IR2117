bc81f0cfac606bf3f01a1e9338a15bf565cddb69
PabloMatilla <al415550@uji.es>
2023-03-23 12:49:34 +0100

    actualización wandering

--- tb3_ws/src/robot_trajectory/src/wandering.cpp
+++ tb3_ws/src/robot_trajectory/src/wandering.cpp
@@ -4,30 +4,34 @@
 #include "rclcpp/rclcpp.hpp"
 #include "geometry_msgs/msg/twist.hpp"
 #include "sensor_msgs/msg/laser_scan.hpp"
+#include "std_msgs/msg/string.hpp"
 #include <cmath>
 
 
 using namespace std::chrono_literals;
 
 void callback_function(const sensor_msgs::msg::LaserScan::SharedPtr msg){
-    std::cout << "Grado 0" << msg;
+    std::cout << "Grado 0" << msg << std::endl;
 }
 
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
   auto node = rclcpp::Node::make_shared("wandering");
-  auto publisher = node->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
+  auto publisher = node->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 0);
   auto subscriber = node->create_subscription<sensor_msgs::msg::LaserScan>("laser_scan", 10, callback_function);
+  std_msgs::msg::s
 
   geometry_msgs::msg::Twist message;
   rclcpp::WallRate loop_rate(10ms);
-
-  message.linear.x = 0;
-  message.angular.z = 0;
-  rclcpp::spin_some(node);
-  loop_rate.sleep();
-  publisher->publish(message);
+  
+  while (rclcpp::ok()){
+    message.linear.x = 0;
+    message.angular.z = 0;
+    rclcpp::spin_some(node);
+    loop_rate.sleep();
+    publisher->publish(message);
+  }
   
   rclcpp::shutdown();
   return 0;
