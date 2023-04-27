bbb98bcd7896aef019d4cfd8ed7060c01cb7e9a3
PabloMatilla <al415550@uji.es>
2023-04-27 13:01:19 +0200

    Add the “radius” parameter

--- tb3_ws/src/olympic/src/rings.cpp
+++ tb3_ws/src/olympic/src/rings.cpp
@@ -10,16 +10,32 @@ int main(int argc, char * argv[])
  rclcpp::init(argc, argv);
  auto node = rclcpp::Node::make_shared("rings");
  auto publisher = node->create_publisher<geometry_msgs::msg::Twist>("/turtle1/cmd_vel", 10);
+ node->declare_parameter("radius", 1.0);
  geometry_msgs::msg::Twist message;
  rclcpp::WallRate loop_rate(500ms);
-
- while (rclcpp::ok()) {
-   message.linear.x = 1;
-   message.angular.z = 1;
+ 
+ double vel_linear = 1;
+ double radius = node->get_parameter("radius").get_parameter_value().get<double>();
+ 
+ // Perimetro
+ double perimetro = 2 * 3.1416 * radius;
+ 
+ // Iteraciones
+ int iteraciones = perimetro/vel_linear/0.5;
+ int count = 0;
+ 
+ while (rclcpp::ok() and count < iteraciones) {
+   message.linear.x = vel_linear;
+   message.angular.z = vel_linear/radius;
    publisher->publish(message);
    rclcpp::spin_some(node);
    loop_rate.sleep();
+   count++;
  }
+ message.linear.x = 0;
+ message.angular.z = 0;
  rclcpp::shutdown();
  return 0;
 }
+
+// ros2 run olympic rings --ros-args --param radius:=0.5
