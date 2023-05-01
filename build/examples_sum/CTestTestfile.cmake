6ea28e37778e738634da4f104409b59a97f91b0d
PabloMatilla <104531022+PabloMatilla@users.noreply.github.com>
2023-05-01 19:33:13 +0200

    Delete build directory

--- build/examples_sum/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pablo/Documentos/IR2117/ros2_ws/src/examples/sum
-# Build directory: /home/pablo/Documentos/IR2117/build/examples_sum
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/examples_topics/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pablo/Documentos/IR2117/ros2_ws/src/examples/topics
-# Build directory: /home/pablo/Documentos/IR2117/build/examples_topics
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
