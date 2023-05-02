fd2baa1757aa0a3c94e37a4a58c1652cc3d09ac5
PabloMatilla <al415550@uji.es>
2023-05-02 12:42:22 +0200

    disponible con un única tortuga

--- tb3_ws/src/olympic/src/rings.cpp
+++ tb3_ws/src/olympic/src/rings.cpp
@@ -5,13 +5,14 @@
 #include "turtlesim/srv/set_pen.hpp"
 #include "turtlesim/srv/teleport_absolute.hpp"
 #include "turtlesim/srv/teleport_relative.hpp"
+#include "turtlesim/srv/set_background.hpp"
 
 using namespace std::chrono_literals;
 using turtlesim::srv::SetPen;
 using turtlesim::srv::TeleportAbsolute;
 using turtlesim::srv::TeleportRelative;
 
-void centrar(std::shared_ptr<rclcpp::Node> node) {
+void centrar(std::shared_ptr<rclcpp::Node> node, double altura) {
      // Service SetPen
  auto client_setpen = node->create_client<SetPen>("/turtle1/set_pen"); 
 
@@ -50,7 +51,7 @@ void centrar(std::shared_ptr<rclcpp::Node> node) {
    // Create a request message for the Teleport service
    auto request_teleport_abs = std::make_shared<TeleportAbsolute::Request>();
    request_teleport_abs->x = 5.5;
-   request_teleport_abs->y = 5.5;
+   request_teleport_abs->y = altura;
    request_teleport_abs->theta = 0;
 
    // Call the set_pen service
@@ -59,71 +60,49 @@ void centrar(std::shared_ptr<rclcpp::Node> node) {
    auto result_teleport_abs = result_future_teleport_abs.get();
 }
 
-int main(int argc, char * argv[])
-{
- rclcpp::init(argc, argv);
- auto node = rclcpp::Node::make_shared("rings");
- auto publisher = node->create_publisher<geometry_msgs::msg::Twist>("/turtle1/cmd_vel", 10);
- node->declare_parameter("radius", 1.0);
- geometry_msgs::msg::Twist message;
- rclcpp::WallRate loop_rate(500ms);
- 
- double vel_linear = 1;
- double radius = node->get_parameter("radius").get_parameter_value().get<double>();
- 
- // Perimetro
- double perimetro = 2 * 3.1416 * radius;
- 
- // Iteraciones
- int iteraciones = perimetro/vel_linear/0.5;
- int count = 0;
- 
- 
- centrar(node);
-
-
- // Service teleport relative
+void teleport_rel(std::shared_ptr<rclcpp::Node> node, double linear, double angular) {
+    // Service teleport relative
  auto client_teleport = node->create_client<TeleportRelative>("/turtle1/teleport_relative"); 
 
   // Wait for the service to be available
   while (!client_teleport->wait_for_service(std::chrono::seconds(1))) {
     if (!rclcpp::ok()) {
       RCLCPP_ERROR(node->get_logger(), "Interrupted while waiting for the service. Exiting.");
-      return 1;
     }
     RCLCPP_INFO(node->get_logger(), "Service not available, waiting again...");
   }
 
    // Create a request message for the Teleport service
    auto request_teleport = std::make_shared<TeleportRelative::Request>();
-   request_teleport->linear = -1;
-   request_teleport->angular = 0;
+   request_teleport->linear = linear;
+   request_teleport->angular = angular;
 
    // Call the set_pen service
    auto result_future_teleport = client_teleport->async_send_request(request_teleport);
    rclcpp::spin_until_future_complete(node, result_future_teleport);
    auto result_teleport = result_future_teleport.get();
+}
 
-
-
- // Service SetPen
+void dibujar(std::shared_ptr<rclcpp::Node> node, std::shared_ptr<rclcpp::Publisher<geometry_msgs::msg::Twist>> publisher, double radius, double vel_linear, int r, int g, int b) {
+ geometry_msgs::msg::Twist message;
+ rclcpp::WallRate loop_rate(500ms);
+    // Service SetPen
  auto client_setpen = node->create_client<SetPen>("/turtle1/set_pen"); 
 
   // Wait for the service to be available
   while (!client_setpen->wait_for_service(std::chrono::seconds(1))) {
     if (!rclcpp::ok()) {
       RCLCPP_ERROR(node->get_logger(), "Interrupted while waiting for the service. Exiting.");
-      return 1;
     }
     RCLCPP_INFO(node->get_logger(), "Service not available, waiting again...");
   }
 
    // Create a request message for the set_pen service
    auto request = std::make_shared<SetPen::Request>();
-   request->r = 255;
-   request->g = 0;
-   request->b = 0;
-   request->width = 3;
+   request->r = r;
+   request->g = g;
+   request->b = b;
+   request->width = 4;
    request->off = false;
 
    // Call the set_pen service
@@ -131,6 +110,13 @@ int main(int argc, char * argv[])
    rclcpp::spin_until_future_complete(node, result_future);
    auto result_setpen = result_future.get();
 
+   // Perimetro
+   double perimetro = 2 * 3.1416 * radius;
+ 
+   // Iteraciones
+   int iteraciones = perimetro/vel_linear/0.5 + 2;
+   int count = 0;
+   
    while (rclcpp::ok() and count < iteraciones) {
     message.linear.x = vel_linear;
     message.angular.z = vel_linear/radius;
@@ -141,8 +127,45 @@ int main(int argc, char * argv[])
   }
   message.linear.x = 0;
   message.angular.z = 0;
-  rclcpp::shutdown();
-  return 0;
+}
+
+int main(int argc, char * argv[])
+{
+ rclcpp::init(argc, argv);
+ auto node = rclcpp::Node::make_shared("rings");
+ auto publisher = node->create_publisher<geometry_msgs::msg::Twist>("/turtle1/cmd_vel", 10);
+ node->declare_parameter("radius", 1.0);
+ rclcpp::WallRate loop_rate(500ms);
+ 
+ double vel_linear = 1;
+ double radius = node->get_parameter("radius").get_parameter_value().get<double>();
+ 
+ // Circulo negro
+ centrar(node, 5.5 + radius/2);
+ dibujar(node, publisher, radius, vel_linear, 0, 0, 0);
+ 
+ // Circulo azul
+ centrar(node, 5.5 + radius/2);
+ teleport_rel(node, -radius*2 * 1.1, 0);
+ dibujar(node, publisher, radius, vel_linear, 0, 0, 255);
+ 
+ // Circulo rojo
+ centrar(node, 5.5 + radius/2);
+ teleport_rel(node, +radius*2 * 1.1, 0);
+ dibujar(node, publisher, radius, vel_linear, 255, 0, 0);
+ 
+ // Circulo amarillo
+ centrar(node, 5.5 - radius/2);
+ teleport_rel(node, -radius * 1.1, 0);
+ dibujar(node, publisher, radius, vel_linear, 255, 255, 0);
+ 
+ // Circulo verde
+ centrar(node, 5.5 - radius/2);
+ teleport_rel(node, radius * 1.1, 0);
+ dibujar(node, publisher, radius, vel_linear, 0, 128, 0);
+ 
+ rclcpp::shutdown();
+ return 0;
 }
 
 // ros2 run olympic rings --ros-args --param radius:=0.5
